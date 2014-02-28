f31d364cf95d92218da089e4293333ee0caaded1
Samuele Santi <redshadow@hackzine.org>
2014-02-28 16:02:32 +0100

    Simple test to make sure tests setup & fixtures are all fine

---
+++ ckan_api_client/tests/test_00_startup_and_fixtures.py
@@ -0,0 +1,20 @@
+"""Make sure tests setup & fixtures are all fine"""
+
+import requests
+
+from .utils.http import check_response_ok, check_response_error
+
+
+def test_site_read(ckan_url):
+    """GET /site_read/ should return 200"""
+
+    api_url = ckan_url('/api/3/action/site_read')
+    response = requests.get(api_url)
+    data = check_response_ok(response)
+    assert data['result'] is True
+
+    ## Call to an invalid URL should return 404
+    response = requests.get(ckan_url('/api/3/action/site_read/something'))
+
+    assert not response.ok
+    assert response.status_code == 404
