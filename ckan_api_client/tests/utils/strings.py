a9e290a97ce13aee7ac600c513b60bd91c0d8ceb
Samuele Santi <redshadow@hackzine.org>
2014-03-05 18:30:49 +0100

    More string generation utilities

--- ckan_api_client/tests/utils/strings.py
+++ ckan_api_client/tests/utils/strings.py
@@ -3,6 +3,7 @@ String generation functions.
 """
 
 import binascii
+import hashlib
 import os
 import random
 import string
@@ -13,5 +14,34 @@ def generate_password(length=20):
 
 
 def generate_random_alphanum(length=10):
+    """Generate a random string, made of ascii letters + digits"""
     charset = string.ascii_letters + string.digits
     return ''.join(random.choice(charset) for _ in xrange(length))
+
+
+def gen_random_id(length=10):
+    """Generate a random id, made of lowercase ascii letters + digits"""
+    charset = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(charset) for _ in xrange(length))
+
+
+def gen_dataset_name():
+    """Generate a random dataset name"""
+    return "dataset-{0}".format(gen_random_id())
+
+
+def gen_picture(s, size=200):
+    """Generate URL to picture from some text hash"""
+    return gen_robohash(s, size)
+
+
+def gen_gravatar(s, size=200):
+    h = hashlib.md5(s).hexdigest()
+    return ('http://www.gravatar.com/avatar/{0}.jpg'
+            '?d=identicon&f=y&s={1}'.format(h, size))
+
+
+def gen_robohash(s, size=200):
+    h = hashlib.md5(s).hexdigest()
+    return ('http://robohash.org/{0}.png?size={1}x{1}&bgset=bg2&set=set1'
+            .format(h, size))
