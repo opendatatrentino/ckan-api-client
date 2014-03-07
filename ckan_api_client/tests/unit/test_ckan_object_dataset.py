13f6cf7809320b7ddc321d2f3acfc4875e072938
Samuele Santi <redshadow@hackzine.org>
2014-03-07 17:49:02 +0100

    [#18] More tests for CkanDataset object

--- ckan_api_client/tests/unit/test_ckan_object_dataset.py
+++ ckan_api_client/tests/unit/test_ckan_object_dataset.py
@@ -9,9 +9,31 @@ def test_ckandataset_creation():
         'title': 'Example Dataset',
         'author': 'Foo Bar',
         'author_email': 'foobar@example.com',
+        'extras': {'foo': 'bar', 'baz': 'SPAM!'},
+        'groups': ['one', 'two', 'three'],
     })
     assert dataset.name == 'example-dataset'
     assert dataset.title == 'Example Dataset'
-    assert dataset.groups == []
-    assert dataset.extras == {}
+    assert dataset.groups == ['one', 'two', 'three']
+    assert dataset.extras == {'foo': 'bar', 'baz': 'SPAM!'}
     assert dataset.resources == []
+
+    assert dataset.serialize() == {
+        'id': None,
+        'name': 'example-dataset',
+        'title': 'Example Dataset',
+        'author': 'Foo Bar',
+        'author_email': 'foobar@example.com',
+        'license_id': None,
+        'maintainer': None,
+        'maintainer_email': None,
+        'notes': None,
+        'owner_org': None,
+        'private': False,
+        'state': 'active',
+        'type': 'dataset',
+        'url': None,
+        'extras': {'foo': 'bar', 'baz': 'SPAM!'},
+        'groups': ['one', 'two', 'three'],
+        'resources': [],
+    }
