da562386a30ac4ce46a5738e69a03ffac4598fd1
Samuele Santi <redshadow@hackzine.org>
2014-03-14 18:40:27 +0100

    Some tests for syncing client

--- ckan_api_client/tests/functional/client_sync/test_simple_harvesting_scenario.py
+++ ckan_api_client/tests/functional/client_sync/test_simple_harvesting_scenario.py
@@ -1,21 +1,49 @@
-# """
-# Test some simple harvesting scenarios
-# """
+"""
+Test some simple harvesting scenarios, generating data on-the-fly
+"""
 
 # import os
+import random
 
 # import pytest
 
 # from ckan_api_client.syncing import CkanDataImportClient
 # from .utils.harvest_source import HarvestSource
+from ckan_api_client.tests.utils.generate import (
+    generate_organization, generate_group, generate_dataset)
 
 
-# HERE = os.path.abspath(os.path.dirname(__file__))
-# DATA_DIR = os.path.join(os.path.dirname(HERE), 'data', 'random')
+def generate_data():
+    organizations = {}
+    groups = {}
+    datasets = {}
 
-# HARVEST_SOURCE_NAME = 'dummy-harvest-source'
+    for i in xrange(3):
+        org = generate_organization()
+        org['id'] = org['name'] = 'org-{0}'.format(i)
+        organizations[org['name']] = org
+
+    for i in xrange(10):
+        grp = generate_group()
+        grp['id'] = grp['name'] = 'group-{0}'.format(i)
+        groups[grp['id']] = grp
+
+    for i in xrange(20):
+        dataset = generate_dataset()
+        dataset['id'] = 'dataset-{0}'.format(i)
+        dataset['groups'] = random.sample(list(groups), random.randint(0, 3))
+        dataset['owner_org'] = random.choice(list(organizations))
+        datasets[dataset['id']] = dataset
+
+    return {
+        'organization': organizations,
+        'group': groups,
+        'dataset': datasets,
+    }
 
 
-# @pytest.fixture(params=['day-{0:02d}'.format(x) for x in xrange(4)])
-# def harvest_source(request):
-#     return HarvestSource(DATA_DIR, request.param)
+def test_simple_harvesting(ckan_client_sync):
+    client = ckan_client_sync
+    data = generate_data()
+
+    client.sync('dummy-source', data)
