35385908af4d0ffee746503e586ba2023720e1ae
Samuele Santi <redshadow@hackzine.org>
2014-03-17 19:04:09 +0100

    Better data generation, more synchronization tests

--- ckan_api_client/tests/functional/client_sync/test_simple_harvesting_scenario.py
+++ ckan_api_client/tests/functional/client_sync/test_simple_harvesting_scenario.py
@@ -10,32 +10,15 @@ import random
 # from ckan_api_client.syncing import CkanDataImportClient
 # from .utils.harvest_source import HarvestSource
 from ckan_api_client.tests.utils.generate import (
-    generate_organization, generate_group, generate_dataset)
+    generate_organization, generate_group, generate_dataset, generate_id)
 
 
 def generate_data():
-    organizations = {}
-    groups = {}
-
-    for i in xrange(3):
-        org = generate_organization()
-        org['id'] = org['name'] = 'org-{0}'.format(i)
-        organizations[org['name']] = org
-
-    for i in xrange(10):
-        grp = generate_group()
-        grp['id'] = grp['name'] = 'group-{0}'.format(i)
-        groups[grp['id']] = grp
+    organizations = generate_organizations(5)
+    groups = generate_groups(10)
 
     datasets = generate_datasets(
         groups=groups, organizations=organizations, amount=20)
-
-    for i in xrange(20):
-        dataset = generate_dataset()
-        dataset['id'] = 'dataset-{0}'.format(i)
-        dataset['groups'] = random.sample(list(groups), random.randint(0, 3))
-        dataset['owner_org'] = random.choice(list(organizations))
-        datasets[dataset['id']] = dataset
 
     return {
         'organization': organizations,
@@ -44,11 +27,29 @@ def generate_data():
     }
 
 
+def generate_organizations(amount=10):
+    organizations = {}
+    for i in xrange(3):
+        org = generate_organization()
+        org['id'] = org['name'] = 'org-{0}'.format(generate_id())
+        organizations[org['name']] = org
+    return organizations
+
+
+def generate_groups(amount=10):
+    groups = {}
+    for i in xrange(10):
+        grp = generate_group()
+        grp['id'] = grp['name'] = 'group-{0}'.format(generate_id())
+        groups[grp['id']] = grp
+    return groups
+
+
 def generate_datasets(groups, organizations, amount=20):
     datasets = {}
     for i in xrange(amount):
         dataset = generate_dataset()
-        dataset['id'] = 'dataset-{0}'.format(i)
+        dataset['id'] = 'dataset-{0}'.format(generate_id())
         dataset['groups'] = random.sample(list(groups), random.randint(0, 3))
         dataset['owner_org'] = random.choice(list(organizations))
         datasets[dataset['id']] = dataset
@@ -57,10 +58,18 @@ def generate_datasets(groups, organizations, amount=20):
 
 def test_simple_harvesting(ckan_client_sync):
     client = ckan_client_sync
+
+    ##------------------------------------------------------------
+    ## Initial import of data
+
     data = generate_data()
+    assert len(data['dataset']) == 20
 
     client.sync('dummy-source', data)
     ## todo: check current status
+
+    ##------------------------------------------------------------
+    ## Add 20 new datasets
 
     new_datasets = generate_datasets(
         groups=data['group'],
@@ -68,11 +77,14 @@ def test_simple_harvesting(ckan_client_sync):
         amount=20)
 
     data['dataset'].update(new_datasets)
+    assert len(data['dataset']) == 40
 
     client.sync('dummy-source', data)
     ## todo: check current status
 
+    ##------------------------------------------------------------
     ## Randomly remove 15 datasets, add 20
+
     todel = random.sample(data['dataset'].keys(), 15)
     for key in todel:
         del data['dataset'][key]
@@ -82,6 +94,34 @@ def test_simple_harvesting(ckan_client_sync):
         organizations=data['organization'],
         amount=20)
     data['dataset'].update(new_datasets)
+    assert len(data['dataset']) == 45
 
     client.sync('dummy-source', data)
     ## todo: check current status
+
+    ##------------------------------------------------------------
+    ## Datasets: -25 +50 (should be 80 now..)
+    ## Organizations: +10
+    ## Groups: +20
+    ##------------------------------------------------------------
+
+    todel = random.sample(data['dataset'].keys(), 25)
+    for key in todel:
+        del data['dataset'][key]
+
+    data['group'].update(generate_groups(20))
+    data['organization'].update(generate_organizations(10))
+
+    new_datasets = generate_datasets(
+        groups=data['group'],
+        organizations=data['organization'],
+        amount=20)
+    data['dataset'].update(new_datasets)
+
+    client.sync('dummy-source', data)
+    ## todo: check current status
+
+    ##------------------------------------------------------------
+    ## Now the tricky thing: we need to apply random
+    ## changes to objects and make sure they are updated
+    ## correctly..
