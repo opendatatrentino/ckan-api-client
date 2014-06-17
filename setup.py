88e7b13bd24a24360b972f16de15c5245cc3cd0b
Samuele Santi <redshadow@hackzine.org>
2014-06-17 18:03:53 +0200

    Version 0.1-beta5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = '0.1-beta4'
+version = '0.1-beta5'
 
 install_requires = [
     "requests",  # For performing HTTP requests
